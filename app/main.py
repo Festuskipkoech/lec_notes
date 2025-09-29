435752fa254c382f7235196d1ad7845b3f81248c
FestusDevs <157395832+Festuskipkoech@users.noreply.github.com>
2025-09-29 19:58:37 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -144,7 +144,7 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=settings.allowed_origins, 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
